648cb5fe5e3ed68e41d81b4f8c841a8b5100c858
Junsung Choi <choiexe1@gmail.com>
2025-05-19 23:18:54 +0900

    theme 적용

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:riverpod_mvvm/app_theme.dart';
 import 'package:riverpod_mvvm/core/router/app_router.dart';
 
 class App extends StatelessWidget {
@@ -7,7 +8,13 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final AppRouter appRouter = AppRouter();
+    final AppTheme appTheme = AppTheme();
 
-    return MaterialApp.router(title: '연습용', routerConfig: appRouter.config());
+    return MaterialApp.router(
+      debugShowCheckedModeBanner: false,
+      title: '연습용',
+      routerConfig: appRouter.config(),
+      theme: appTheme.lightTheme,
+    );
   }
 }
