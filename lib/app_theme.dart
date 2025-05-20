e70b4314b1a35b2074daee8e722cc80bac7a1d79
Junsung Choi <choiexe1@gmail.com>
2025-05-20 23:29:37 +0900

    theme

--- lib/app_theme.dart
+++ lib/app_theme.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppTheme {
   ThemeData get lightTheme => ThemeData(
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+    focusColor: Colors.grey.withValues(alpha: 0.5),
     textTheme: const TextTheme(
       displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
       bodyLarge: TextStyle(fontSize: 18),
